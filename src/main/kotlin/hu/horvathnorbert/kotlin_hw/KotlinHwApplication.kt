7217229eb01243e8909f71ee4029f24f5ff00087
Horvath Norbert <horvath.norbert@autsoft.hu>
2020-03-21 16:01:04 +0100

    Initial commit

---
+++ src/main/kotlin/hu/horvathnorbert/kotlin_hw/KotlinHwApplication.kt
@@ -0,0 +1,11 @@
+package hu.horvathnorbert.kotlin_hw
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinHwApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinHwApplication>(*args)
+}
---
+++ src/test/kotlin/hu/horvathnorbert/kotlin_hw/KotlinHwApplicationTests.kt
@@ -0,0 +1,13 @@
+package hu.horvathnorbert.kotlin_hw
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinHwApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
